43e9b1b32e53b297619b18d4b97eb4328337214c
Vaz Allen <vaz@tryptid.com>
2012-05-30 09:37:32 -0700

    More initial stuff.

--- setup.py
+++ setup.py
@@ -1,43 +1,59 @@
+import distribute_setup
+distribute_setup.use_setuptools()
+
 from setuptools import setup, find_packages
 
 version = '0.1'
 
-setup(name='speck',
-      version=version,
+setup(
+    name='speck',
+    version=version,
 
-      description="Speck makes python testing better.",
-      long_description="",
+    description="Speck makes python testing better.",
+    long_description="",
 
-      author='Vaz',
-      author_email='vaz@tryptid.com',
-      url='http://github.com/vaz/speck',
-      license='WTFPL',
+    author='Vaz',
+    author_email='vaz@tryptid.com',
+    url='http://github.com/vaz/speck',
+    license='WTFPL',
 
-      install_requires=[
-      ],
+    install_requires=[
+    ],
 
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+    test_suite='nose.collector',
+    tests_require = [
+        'nose',
+        'coverage',
+        'pinocchio==0.2',
+    ],
 
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
+    dependency_links = [
+        'https://github.com/unpluggd/pinocchio/tarball/0.2#egg=pinocchio-0.2',
+    ],
 
-      classifiers=[
-          "Development Status :: 1 - Planning",
-          "Environment :: Console",
-          "Intended Audience :: Developers",
-          "Intended Audience :: Religion",
-          "License :: Freeware",
-          "Natural Language :: English",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.6",
-          # TODO: Python 3,
-          "Topic :: Software Development :: Testing"
+    entry_points={
+        'console_scripts': [
+            'speck = speck.runner:main',
+        ]
+    },
 
-      ],
-      keywords='python test unit spec expectation should matcher',
-      )
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=True,
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Religion",
+        "License :: Freeware",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        # TODO: Python 3,
+        "Topic :: Software Development :: Testing"
+    ],
+    keywords='python test unit spec expectation should matcher',
+)
