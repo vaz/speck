6a06e1b211f71219fb159153de0b3a5366619213
Vaz Allen <vaz@tryptid.com>
2012-08-01 10:02:17 -0700

    Updated examples again.

--- examples/matchers.py
+++ examples/matchers.py
@@ -9,12 +9,14 @@ def main():
     _(x).should.be(x)
     _(x).should.be.less_than(1000)
     _(x).should.be_less_than(1000)
+    _(x).should_be_less_than(1000)
     _(x).should.be < 1000
     _(x).should_not.be > 1000
     _(x).should.not_be > 1000
     _(x).should.not_be_greater_than_or_equal(1000)
     _(x).should.not_be_greater_than_or_equal_to(1000)
     _(x).should.not_be('dogs')
+    _(x).should_not_be('dogs')
 
     try:
         _(x).should.not_be(x) # raises speck.exception.ExpectationNotMet: NOT 1 is 1
